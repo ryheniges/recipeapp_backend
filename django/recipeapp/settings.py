119adfa2ddb6531334ac628f103ca027b269efdd
Ryan Heniges <henigesr@gmail.com>
2024-08-13 20:50:44 -0400

    [MISC] Add .pyc files to gitignore and remove said files

--- django/recipeapp/settings.py
+++ django/recipeapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "recipe_base.apps.RecipeBaseConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
