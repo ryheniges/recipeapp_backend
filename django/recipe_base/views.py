7464b46c3f44d55b4d5d16c97e2f3aab2d14fdcf
Ryan Heniges <henigesr@gmail.com>
2024-08-17 14:57:43 -0400

    [TESTING] Generic views

--- django/recipe_base/views.py
+++ django/recipe_base/views.py
@@ -1,28 +1,29 @@
 from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from django.template import loader
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("recipe_base/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "recipe_base/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "recipe_base/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "recipe_base/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "recipe_base/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "recipe_base/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "recipe_base/results.html"
 
 
 def vote(request, question_id):
